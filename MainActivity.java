ba0ecff998d5cd24425fbdc55c4df5bf9da0c568
dream78671 <44059254+dream78671@users.noreply.github.com>
2018-11-29 22:12:26 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,53 @@
+package com.jeff.umar.db;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String url = "jdbc:mysql://194.168.155.138:3306/test";
+    private static final String user = "user";
+    private static final String pass = "password";
+
+    public void testDB() {
+        TextView tv = (TextView)this.findViewById(R.id.Forename);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, pass);
+            /* System.out.println("Databaseection success"); */
+
+            String result = "Database connection success\n";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM test.users where forename like 'U%'");
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            while(rs.next()) {
+                result += rsmd.getColumnName(1) + ": " + rs.getInt(1) + "\n";
+                result += rsmd.getColumnName(2) + ": " + rs.getString(2) + "\n";
+                result += rsmd.getColumnName(3) + ": " + rs.getString(3) + "\n";
+            }
+            tv.setText(result);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            tv.setText(e.toString());
+        }
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testDB();
+    }
+}
+
